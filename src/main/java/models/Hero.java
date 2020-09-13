a3805defbbc2a12e24af05c53f7bbbacaeb60a51
Ken-muigai <kenmuigai980@gmail.com>
2020-09-13 15:02:29 +0300

    Initial commit

---
+++ src/main/java/models/Hero.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.SplittableRandom;
+
+public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+
+    public Hero(String name, int age, String specialPower, String weakness){
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
+}
