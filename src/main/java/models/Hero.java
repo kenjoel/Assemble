802564612dfffa39939695a7b418348bde526bfe
Ken-muigai <kenmuigai980@gmail.com>
2020-09-13 15:29:06 +0300

    adding predefined squads

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -8,13 +8,26 @@ public class Hero {
     private String specialPower;
     private String weakness;
 
-    public Hero(String name, int age, String specialPower, String weakness){
+    public Hero(String name, int age, String specialPower, String weakness) {
         this.name = name;
         this.age = age;
         this.specialPower = specialPower;
         this.weakness = weakness;
     }
 
+    public String getName(){
+        return name;
+    }
 
+    public int getAge(){
+        return age;
+    }
 
+    public  String getSpecialPower(){
+        return specialPower;
+    }
+
+    public String getWeakness(){
+        return weakness;
+    }
 }
