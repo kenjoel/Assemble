4ff74a07985e32eb2a7c584507785ad7e6b1bb46
Ken-muigai <kenmuigai980@gmail.com>
2020-09-14 08:47:39 +0300

    squad update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,15 +12,30 @@ public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocreate this
         staticFileLocation("/public");
 
-        //get: show new post form
+        //get: index route which is base route
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //route to form to create squad
         get("/form", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //post route after its created
+        post("/newhero", (request, response) -> {
+            String name = request.queryParams("name");
+            int size = Integer.parseInt(request.queryParams("size"));
+            String cause = request.queryParams("cause");
+
+            Squads squad = new Squads(name,cause,size);
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("squad", squad);
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
--- src/main/java/Squads.java
+++ src/main/java/Squads.java
@@ -5,9 +5,11 @@ import java.util.Map;
 public class Squads {
     private String name;
     private String cause;
+    private int size;
     ArrayList<Object> members = new ArrayList<>(); //can essentially add squad members
 
-    public Squads(String name, String cause){
+    public Squads(String name, String cause, int size){
+        this.size = size;
         this.name = name;
         this.cause = cause;
     }
