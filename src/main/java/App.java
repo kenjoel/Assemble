e1857654dcf5f37bd546654b0ff8de66a61e8f93
Ken-muigai <kenmuigai980@gmail.com>
2020-09-14 07:21:49 +0300

    frontend updated

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,17 +14,7 @@ public class App {
 
         //get: show new post form
         get("/", (req, res) -> {
-            Hero hero1 =  new Hero("Vilgax", 22, "Heat Waves", "Water");
-            Hero hero2 = new Hero("WildMat",30,"Alien Vision and Flight","Broccoli");
-            Hero hero4 = new Hero("Titanium",1000,"Strength And Durability","Slow");
-            Hero hero3 = new Hero("GreyMatter",2000,"Super Intelligence","KumFoolery");
-            Hero hero5 = new Hero("Ghost Freak",200,"Invisibility","GhostBusters");
-            Hero hero6 = new Hero("Alien X",70,"Time Travel and Manipulation","None");
-
             Map<String, Object> model = new HashMap<>();
-
-            ArrayList<Hero> master = Hero.getAll();
-            model.put("Heros", master);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
